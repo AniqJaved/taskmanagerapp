a15b08dbac1bbeb6183efd7b95aef7cebc181688
Aniq Javed <2aniqqjavedd4493@gmail.com>
2022-08-07 01:43:49 +0500

    User,Task Update added with specifying fields that can be updated

--- src/index.js
+++ src/index.js
@@ -23,6 +23,28 @@ app.post('/users', async (req,res) => {
     }
 })
 
+//Update User
+
+app.patch('/users/:id', async (req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["name","age","email","password"]
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if(!isValidOperation){
+        res.status(201).send("Invalid Updates")
+    }
+
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+        if(!user){
+            res.status(404).send()
+        }
+        res.send(user)
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
+})
+
 // Find Users
 app.get('/users', async (req,res)=>{
 
@@ -43,7 +65,6 @@ app.get('/users/:id', async (req,res)=>{  // :id is used to grab the id which us
         const user = await User.findById(_id)
         
         if(!user){
-            console.log("No user")
             res.status(404).send()
         }
         res.status(201).send(user)
@@ -64,6 +85,28 @@ app.post('/tasks', async (req,res) => {
         res.status(400).send(err)
     }
     
+})
+
+//Update task
+
+app.patch('/tasks/:id', async (req,res)=>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ["description","completed"]
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if(!isValidOperation){
+        res.status(201).send("Invalid Updates")
+    }
+
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+        if(!task){
+            res.status(404).send()
+        }
+        res.send(task)
+    }
+    catch(err){
+        res.status(400).send(err)
+    }
 })
 
 
