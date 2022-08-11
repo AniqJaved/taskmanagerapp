086dd9347342da3a70efe1db4d9b1f369d7aae41
Aniq Javed <2aniqqjavedd4493@gmail.com>
2022-08-12 00:49:08 +0500

    Middleware for website mantienece mode

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ require('./db/mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
+//This acts a middleware for the website when in mantienece mode
+app.use((req,res,next)=>{
+    res.status(503).send('Site is currently down please check back later');
+})
+
 // This line converts the request to json format
 app.use(express.json())
 
